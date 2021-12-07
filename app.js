0e534bb37381575533113641434c1e5bb4c6767a
user2695 <53275443+user2695@users.noreply.github.com>
2021-12-07 16:33:53 +0530

    Basic Todo app

---
+++ app.js
@@ -0,0 +1,57 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require( __dirname + "/date.js");
+
+console.log(date);
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.get("/about",function(req, res){
+    res.render("about");
+
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+
+        items.push(item);
+        res.redirect("/");
+    }       
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let items = req.body.newItems;
+    workItems.push(items);
+    res.redirect("/work");
+});
+
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
